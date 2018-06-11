e581ef67b142d8fefbe962cbbef2e48c827c32f5
Natalia Kowalczyk <melitele@code42day.com>
2018-06-10 18:12:31 -0600

    wip

--- example/common.js
+++ example/common.js
@@ -25,21 +25,17 @@ function samplePins(srv, mp) {
   mp.zoom(9);
   mp.addStyle(pinsStyle);
   for (i = 0; i <= 8; i += 1) {
-    srv.marker_({
+/*    srv.marker_({
       map: mp,
-      layer: 'marker',
+      layer: 'compass',
       properties: {
-        path: 'forward_closed_arrow',
-        strokeColor: '#0074D9', // azure
-        strokeWeight: 5,
-        rotation: 45 * i,
-        scale: 5
+        rotation: 45 * i
       },
       geometry: {
         type: 'Point',
         coordinates: [i / 8, 3 / 8]
       },
-    });
+    });*/
     mk = srv.artifact({
       map: mp,
       layer: 'circle',
@@ -51,36 +47,40 @@ function samplePins(srv, mp) {
         coordinates: [i / 8, 1 / 8]
       }
     });
-    srv.marker_({
+/*    srv.marker_({
       map: mp,
       layer: 'marker',
       properties: {
-        color: 'orange',
-        path: 'M 2 0 L 2 0.28125 L 2 13.6875 L 2 14 L 0.59375 14 L 0 14 L 0 14.59375 L 0 15.375 L 0 16 '
-          + 'L 0.59375 16 L 4.40625 16 L 5 16 L 5 15.375 L 5 14.59375 L 5 14 L 4.40625 14 L 3 14 L 3 13.6875 '
-          + 'L 3 9 L 16 5.5 L 3 2 L 3 0.28125 L 3 0 L 2.6875 0 L 2.3125 0 L 2 0 z ',
-        strokeColor: 'orange',
-        strokeWeight: 1,
-        scale: (i + 1) / 3,
-        anchor: [1, 16]
+        scale: (i + 1) / 3
       },
       geometry: {
         type: 'Point',
         coordinates: [i / 8, -1 / 8],
       }
-    });
-    mk = srv.marker_({
-      map: mp,
-      layer: 'marker',
-      properties: {
-        url: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNiIgaGVpZ2h0PSIzMCI+PHBhdGggZD0iTTAgMEgyNlYyNkgxNkwxMyAzMEwxMCAyNkgwWiIgZmlsbD0iI2Y4MDAxMiIvPjxwYXRoIGQ9Ik0yIDJIMjRWMjRIMloiIGZpbGw9IiNmZmYiLz48cGF0aCBkPSJNMjEuOCAxMC40TDcuNSA2LjZWNC40SDYuNFYxOS44SDQuMlYyMkg5LjdWMTkuOEg3LjVWMTQuM1oiIGZpbGw9IiNmODAwMTIiLz48L3N2Zz4=',
-        size: [26, 30]
-      },
-      geometry: {
-        type: 'Point',
-        coordinates: [i / 8, -3 / 8],
-      }
-    });
+    });*/
+    if (i < 4) {
+      mk = srv.marker_({
+        map: mp,
+        layer: 'marker-1',
+        geometry: {
+          type: 'Point',
+          coordinates: [i / 8, -3 / 8],
+        }
+      });
+    }
+    /*else {
+      mk = srv.marker_({
+        map: mp,
+        layer: 'marker-2',
+        properties: {
+          image: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNiIgaGVpZ2h0PSIzMCI+PHBhdGggZD0iTTAgMEgyNlYyNkgxNkwxMyAzMEwxMCAyNkgwWiIgZmlsbD0iI2Y4MDAxMiIvPjxwYXRoIGQ9Ik0yIDJIMjRWMjRIMloiIGZpbGw9IiNmZmYiLz48cGF0aCBkPSJNMjEuOCAxMC40TDcuNSA2LjZWNC40SDYuNFYxOS44SDQuMlYyMkg5LjdWMTkuOEg3LjVWMTQuM1oiIGZpbGw9IiNmODAwMTIiLz48L3N2Zz4='
+        },
+        geometry: {
+          type: 'Point',
+          coordinates: [i / 8, -3 / 8],
+        }
+      });
+    }*/
   }
 }
 
--- example/style.js
+++ example/style.js
@@ -14,26 +14,57 @@ var pinsStyle = {
       'circle-stroke-width': 5
     }
   }, {
-      id: 'marker',
+      id: 'compass',
       type: 'symbol',
       layout: {
-        'icon-image': {
-          property: 'path',
-          type: 'identity'
-        },
-        'icon-offset': {
-          property: 'offset',
-          type: 'identity'
-        },
+        'icon-image': 'symbol:forward_closed_arrow',
         'icon-rotate': {
           property: 'rotation',
           type: 'identity'
-        },
-        'icon-size': {
-          property: 'scale',
-          type: 'identity'
         }
+      },
+      paint: {
+        'icon-color': '#0074D9' // azure
       }
+  }, {
+    id: 'flag',
+    type: 'symbol',
+    layout: {
+      'icon-image': 'path:M 2 0 L 2 0.28125 L 2 13.6875 L 2 14 L 0.59375 14 L 0 14 L 0 14.59375 L 0 15.375 L 0 16 '
+        + 'L 0.59375 16 L 4.40625 16 L 5 16 L 5 15.375 L 5 14.59375 L 5 14 L 4.40625 14 L 3 14 L 3 13.6875 '
+        + 'L 3 9 L 16 5.5 L 3 2 L 3 0.28125 L 3 0 L 2.6875 0 L 2.3125 0 L 2 0 z',
+      'icon-offset': [1, 16],
+      'icon-size': {
+        property: 'scale',
+        type: 'identity'
+      }
+    },
+    paint: {
+      'icon-color': 'orange',
+    }
+  }, {
+    id: 'marker-1',
+    type: 'symbol',
+    metadata: {
+      'facade:icon-size': [26, 30]
+    },
+    layout: {
+      'icon-image': 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNiIgaGVpZ2h0PSIzMCI+PHBhdGggZD0iTTAgMEgyNlYyNkgxNkwxMyAzMEwxMCAyNkgwWiIgZmlsbD0iI2Y4MDAxMiIvPjxwYXRoIGQ9Ik0yIDJIMjRWMjRIMloiIGZpbGw9IiNmZmYiLz48cGF0aCBkPSJNMjEuOCAxMC40TDcuNSA2LjZWNC40SDYuNFYxOS44SDQuMlYyMkg5LjdWMTkuOEg3LjVWMTQuM1oiIGZpbGw9IiNmODAwMTIiLz48L3N2Zz4=',
+      'icon-offset': [0, -45],
+      'icon-size': {
+        property: 'scale',
+        type: 'identity'
+      }
+    }
+  }, {
+    id: 'marker-2',
+    type: 'symbol',
+    layout: {
+      'icon-image': {
+        property: 'image',
+        type: 'identity'
+      }
+    }
   }]
 };
 
