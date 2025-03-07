441963698e4388f92ec6056da43df34ed7d07b0e
Natalia Kowalczyk <melitele@code42day.com>
2025-03-06 20:21:07 -0700

    implement shape that can be added to a source predefined in a map style

---
+++ test/service/mapbox/feature.js
@@ -0,0 +1,38 @@
+const { describe, it } = require('node:test');
+const feature = require('../../../lib/service/mapbox/feature');
+
+describe('feature', () => {
+  it('create', () => {
+    const data = {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [0, 0]
+      }
+    };
+    const ft = feature({
+      source: '_source',
+      data
+    });
+    ft.should.have.property('_data');
+    ft._data.should.deepEqual(data);
+    ft.should.have.property('_source', '_source');
+  });
+
+  it('position', () => {
+    const data = {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [0, 0]
+      }
+    };
+    const ft = feature({
+      source: '_source',
+      data
+    });
+    ft.position().should.deepEqual([0, 0]);
+    ft.position([1, 1]);
+    ft.position().should.deepEqual([1, 1]);
+  });
+});
