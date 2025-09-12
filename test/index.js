3097a5e68aaac2769681dff16005de0f58e5bc04
melitele <melitele@furkot.com>
2025-09-11 21:08:44 -0600

    remove `projection`

--- test/index.js
+++ test/index.js
@@ -4,6 +4,6 @@ const maps = require('../lib');
 describe('maps', () => {
   it('init', t => {
     const m = maps.init();
-    ['map', 'projection', 'util'].forEach(prop => t.assert.ok(prop in m, `map should have property: ${prop}`));
+    ['map', 'util'].forEach(prop => t.assert.ok(prop in m, `map should have property: ${prop}`));
   });
 });
